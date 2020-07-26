4157286deb539fe9d4f8131ed548743f554b701d
Blacit <vgilenko@bk.ru>
2020-07-26 13:55:35 +0300

    New processing and Java version

---
+++ src/main/java/Departments.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Departments {
+
+    public static void depart(String[] args) throws IOException {
+
+    }
+}
---
+++ src/main/java/Employees.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Employees {
+
+    public static void employ(String[] args) throws IOException {
+
+    }
+}
