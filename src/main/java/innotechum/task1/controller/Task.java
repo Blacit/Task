a92d6892b646cd7bc0eb2dad5488f9d72026ad32
Blacit <vgilenko@bk.ru>
2020-08-07 15:45:16 +0300

    add

--- src/main/java/innotechum/task1/controller/Task.java
+++ src/main/java/innotechum/task1/controller/Task.java
@@ -42,21 +42,29 @@ public class Task implements AutoCloseable {
                 key = "Второй отдел имеет большую среднюю зп";
             }
             System.out.println(empswap(departments, key, avgEmp));*/
-            Departament department1= departments.get("Первый");
-            Departament department2= departments.get("Второй");
+            Departament department1 = departments.get("Первый");
+            Departament department2 = departments.get("Второй");
 
-            List<Employee> empl = new ArrayList<>(); // лист, в котором будут храниться сотрудники для перевода
-                if (avgEmp.get(0).compareTo(avgEmp.get(1)) > 0) { // если средняя ЗП в первом отделе больше
-                    empl.addAll(
+            System.out.println(department1); // Department{name='Первый', employeeList=[Employee{salary=15000.13, name='Кошкин'}, Employee{salary=13000.18, name='Петрович Котович Кот'}, Employee{salary=50000, name='Сик'}, Employee{salary=30000.2, name='Сидоров'}, Employee{salary=40000, name='Пушкин'}]}
+            System.out.println(department2); // Department{name='Второй', employeeList=[Employee{salary=10000, name='Петров'}, Employee{salary=30000, name='Петров'}]}
+            System.out.println(avgEmp.get(0)); // 29600.102
+            System.out.println(avgEmp.get(1)); //20000
+
+            List<Employee> employeeList  = new ArrayList<>(); // лист, в котором будут храниться сотрудники для перевода
+            if (avgEmp.get(0).compareTo(avgEmp.get(1)) > 0) { // если средняя ЗП в первом отделе больше
+                employeeList .addAll(
                         department1.getEmployeeList().stream() // значит проходим по первому листу и сравниваем
-                                .filter(emp -> emp.getSalary().compareTo(avgEmp.get(0)) > 0 && emp.getSalary().compareTo(avgEmp.get(1)) < 0)
+                                .filter(emp -> emp.getSalary().compareTo(avgEmp.get(0)) < 0 && emp.getSalary().compareTo(avgEmp.get(1)) > 0)
                                 .collect(Collectors.toList()));
             } else if (avgEmp.get(0).compareTo(avgEmp.get(1)) < 0) {
-                    System.out.println("hello");
+                employeeList .addAll(
+                        department2.getEmployeeList().stream() // значит проходим по первому листу и сравниваем
+                                .filter(emp -> emp.getSalary().compareTo(avgEmp.get(0)) < 0 && emp.getSalary().compareTo(avgEmp.get(1)) > 0)
+                                .collect(Collectors.toList()));
             } else {
-                    System.out.println("hello");
+                System.out.println("Нет сотрудника для увеличения ср зп");
             }
-            for (Employee name : empl) {
+            for (Employee name : employeeList ) {
                 System.out.println(name);
             }
 
@@ -70,7 +78,8 @@ public class Task implements AutoCloseable {
             System.out.println("Читаемый файл закрыт");
         }
     }
-            // Есть какие-то правила по порядку написания методов?
+
+    // Есть какие-то правила по порядку написания методов?
     public static boolean check(String line) {
         String regex = "[А-Яа-яЁёA-Za-z\\s]+";
         if (!(line = line.trim()).isEmpty()) {
